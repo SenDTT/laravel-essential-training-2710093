02fbbb21c405f01aa3a2b9c1f96c9f267f0c0743
SenDoan <mariathanhsen@gmail.com>
2025-06-16 16:46:33 -0500

    Greeting

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,16 @@
+<x-base>
+
+<x-slot:title>
+    Greeting Page
+</x-slot>
+
+<x-slot name="content">
+    <h1>Welcome to the Greeting Page!</h1>
+    <p>This page is designed to greet users with a friendly message.</p>
+    <p>Feel free to explore and enjoy your stay!</p>
+    <p>Have a great day!</p>
+    <p>Thank you for visiting!</p>
+    <p>Keep smiling!</p>  
+</x-slot>
+
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetingController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', GreetingController::class . '@index')
+    ->name('greeting.index');
